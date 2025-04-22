823591143cf53a33977bfa475c873e2671cea48a
kkatka <kkatka@mail.ru>
2025-04-22 22:53:38 +0300

    1st commit add in serializers.py

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,15 +1,21 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from .models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    lessons_count = SerializerMethodField()
+
     class Meta:
         model = Course
         fields = '__all__'
 
-class LessonSerializer(ModelSerializer):
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
 
+
+class LessonSerializer(ModelSerializer):
 
     class Meta:
         model = Lesson
