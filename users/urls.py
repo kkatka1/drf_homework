9d9be10d30f0b8ad5af939a571d960f1b55aa659
kkatka <kkatka@mail.ru>
2025-04-29 17:02:50 +0300

    fix: Correct user URLs and permission fixtures

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,8 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
 from users.apps import UsersConfig
-from users.views import PaymentViewSet, UserCreateAPIView
+from users.views import PaymentViewSet, UserCreateAPIView, UserListAPIView, UserRetrieveAPIView, UserUpdateAPIView, \
+    UserDestroyAPIView
 
 app_name = UsersConfig.name
 
@@ -15,14 +16,11 @@ router.register(r"payments", PaymentViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("register/", UserCreateAPIView.as_view(), name="register"),
-    path(
-        "login/",
-        TokenObtainPairView.as_view(permission_classes=(AllowAny,)),
-        name="login",
-    ),
-    path(
-        "token/refresh/",
-        TokenRefreshView.as_view(permission_classes=(AllowAny,)),
-        name="token_refresh",
-    ),
+    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="login"),
+    path("list/", UserListAPIView.as_view(), name="user-list"),
+    path("<int:pk>/", UserRetrieveAPIView.as_view(), name="user-detail"),
+    path("<int:pk>/update", UserUpdateAPIView.as_view(), name="users_update"),
+    path("delete/<int:pk>/", UserDestroyAPIView.as_view(), name="users_delete"),
+
+    path("token/refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name="token_refresh")
 ]
