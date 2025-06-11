8cd779bdf9fce709790a7e404553bcb20bb77878
kkatka <kkatka@mail.ru>
2025-06-11 20:32:57 +0300

    Настроено окружение, исправлен ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+
 
 INSTALLED_APPS = [
     "django.contrib.admin",
